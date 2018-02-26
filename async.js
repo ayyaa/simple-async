7cde8c3631b0a8b804bf390da60fbb1fa8db262d
Tsurayya <ayyaa.ats12@gmail.com>
2018-02-26 12:01:09 +0700

    Update async.js

--- async.js
+++ async.js
@@ -1,21 +1,23 @@
 var fs = require ('fs');
+var path = require ('path');
 
-fs.rename('serial.txt', 'serial-dua.txt', function (error) {
-});
-
-fs.readFile('serial-dua.txt','utf8', function (error, contents) {
-	console.log(contents);
-});
-
-fs.writeFile('message.txt', 'Hello Tsurayya', function (err) {
+fs.writeFile(path.join(path.dirname(__filename), 'message.txt'), 'Hello Tsurayya, How Are you ?', function (err) {
   if (err) throw err;
   console.log('It\'s saved!');
 });
 
-fs.readFile('message.txt','utf8', function (error, contents) {
+fs.rename('serial.txt', 'serial-dua.txt', function (error) {
+});
+
+fs.readFile(path.join(path.dirname(__filename), 'simple.txt'),'utf8', function (error, contents) {
 	console.log(contents);
 });
 
-fs.readFile('simple.txt','utf8', function (error, contents) {
+fs.readFile(path.join(path.dirname(__filename), 'serial-dua.txt'), 'utf8', function (error, contents) {
 	console.log(contents);
 });
+
+fs.readFile(path.join(path.dirname(__filename), 'message.txt'), 'utf8', function (error, contents) {
+	console.log(contents);
+});
+
